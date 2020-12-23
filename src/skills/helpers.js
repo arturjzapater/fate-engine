626e9e293a6f17d2eec77641360cf59cac22ed5c
arturjzapater <zapater.artur@gmail.com>
2020-12-23 10:22:11 +0100

    moved skill helpers to separate file

---
+++ src/skills/helpers.js
@@ -0,0 +1,28 @@
+const S = require('sanctuary')
+const { summation } = require('helpers')
+
+const dropSummation = S.compose(S.drop)(summation)
+
+const makeRow = num => S.pipe([
+	dropSummation(num),
+	S.chain(S.take(num + 1)),
+	S.fromMaybe([]),
+])
+
+const checkLength = num =>
+	list =>
+		summation(num) > list.length
+			? S.Nothing
+			: S.Just(list)
+
+const makeTiers = (ladder, cap) =>
+	skillList =>
+		Array.from(
+			{ length: cap },
+			(_, i) => [ ladder[cap - i], makeRow(i)(skillList) ]
+		)
+
+module.exports = {
+	checkLength,
+	makeTiers,
+}
--- src/skills/index.js
+++ src/skills/index.js
@@ -1,26 +1,6 @@
 const S = require('sanctuary')
-const { shuffle, summation } = require('helpers')
-
-const checkLength = num =>
-	list =>
-		summation(num) > list.length
-			? S.Nothing
-			: S.Just(list)
-
-const dropSummation = S.compose(S.drop)(summation)
-
-const makeRow = num => S.pipe([
-	dropSummation(num),
-	S.chain(S.take(num + 1)),
-	S.fromMaybe([]),
-])
-
-const makeTiers = (ladder, cap) =>
-	skillList =>
-		Array.from(
-			{ length: cap },
-			(_, i) => [ ladder[cap - i], makeRow(i)(skillList) ]
-		)
+const { shuffle } = require('helpers')
+const { checkLength, makeTiers } = require('./helpers')
 
 const makeList = ({ list, ladder }) =>
 	(input = 4) =>
